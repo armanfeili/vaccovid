8990c17beadf719fd3616192c53a9867047dfaf3
Arman Feili <armanfc.enter@yahoo.com>
2020-11-14 05:12:15 +0330

    buji

--- client/src/components/news/news.jsx
+++ client/src/components/news/news.jsx
@@ -127,6 +127,7 @@ export class NewsComponent extends Component {
         this.setState({ active_btn: "health" });
         this.setState({ page: 0 });
         // const health = 
+
         await this.props.getHealthNews(0);
         // console.log(health);
         const allNews = this.props.news.news;
