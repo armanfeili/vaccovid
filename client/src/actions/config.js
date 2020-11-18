7241a87fbb7e3e47aff22e3131b78ed8fec0a0fc
Arman Feili <armanfc.enter@yahoo.com>
2020-11-18 22:32:06 +0330

    bugs-4

--- client/src/actions/config.js
+++ client/src/actions/config.js
@@ -2,6 +2,6 @@ export const secretData = {
     // url for developement
     // baseURL: "http://127.0.0.1:5000/api",
     baseURL: "/api",
-    // url for production
+     // url for production
     // baseURL: "http://vaccovid.live/api",
 };
