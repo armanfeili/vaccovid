c7f6e7c141221c1e6f1b386af16cb0a5a2f2c86f
Arman Feili <armanfc.enter@yahoo.com>
2020-11-23 21:21:52 +0330

    backend-vaccine

---
+++ app/src/routes/vaccine.ts
@@ -0,0 +1,162 @@
+import Express from "express";
+import { getManager } from "typeorm";
+import { OwidData } from "../db/models/OwidData";
+import {
+    convertVaccineData, updateVaccine,
+    getAllVaccines, getAllPhase_PreClinical_Vaccines, getAllPhase_one_Vaccines, getAllPhase_two_Vaccines, getAllPhase_three_Vaccines, getAllPhase_four_Vaccines, get_FDA_Approved_Vaccines,
+    getAllTreatments, getAllPreClinicalTreatments, getAllClinicalTreatments, getAll_FDA_Approved_Treatments,
+    getVaccineCategoryBased, getTreatmentCategoryBased
+} from "../utils/vaccineAndTreatment";
+
+const Router = Express.Router();
+
+Router.post("/download-and-convert-vaccine-data", async (req, res) => {
+    try {
+        res.status(200).json(await convertVaccineData());
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.put("/update-vaccine", async (req, res) => {
+    try {
+        const vacData = await updateVaccine();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+/////////////////////////////////////////////////////
+///////////////   Vaccines   ////////////////////////
+/////////////////////////////////////////////////////
+
+Router.get("/get-all-vaccines", async (req, res) => {
+    try {
+        const vacData = await getAllVaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-vaccines-pre-clinical", async (req, res) => {
+    try {
+        const vacData = await getAllPhase_PreClinical_Vaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-vaccines-phase-i", async (req, res) => {
+    try {
+        const vacData = await getAllPhase_one_Vaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-vaccines-phase-ii", async (req, res) => {
+    try {
+        const vacData = await getAllPhase_two_Vaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-vaccines-phase-iii", async (req, res) => {
+    try {
+        const vacData = await getAllPhase_three_Vaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-vaccines-phase-iv", async (req, res) => {
+    try {
+        const vacData = await getAllPhase_four_Vaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-fda-approved-vaccines", async (req, res) => {
+    try {
+        const vacData = await get_FDA_Approved_Vaccines();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+/////////////////////////////////////////////////////
+///////////////   Treatments   //////////////////////
+/////////////////////////////////////////////////////
+
+Router.get("/get-all-treatment", async (req, res) => {
+    try {
+        const vacData = await getAllTreatments();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-treatment-pre-clinical", async (req, res) => {
+    try {
+        const vacData = await getAllPreClinicalTreatments();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-treatment-clinical", async (req, res) => {
+    try {
+        const vacData = await getAllClinicalTreatments();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-all-fda-approved-treatment", async (req, res) => {
+    try {
+        const vacData = await getAll_FDA_Approved_Treatments();
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+/////////////////////////////////////////////////
+///////////   category based    /////////////////
+/////////////////////////////////////////////////
+
+Router.get("/get-vaccines/:category", async (req, res) => {
+    try {
+        const vacData = await getVaccineCategoryBased(req.params.category);
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+Router.get("/get-treatments/:category", async (req, res) => {
+    try {
+        const vacData = await getTreatmentCategoryBased(req.params.category);
+        res.status(200).json(vacData);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+export default Router;
